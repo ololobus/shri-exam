8fe95b50985ce12a7e706839b6f3c1738c366627
Alex K <alex.ololo@yandex.ru>
2013-10-10 23:49:57 +0400

    Подключение виджетов Твиттера и ВК

--- js/view.static.js
+++ js/view.static.js
@@ -15,7 +15,21 @@ var StaticView = Backbone.View.extend({
   },
   
   renderSocial: function() {
-    this.$el.html("");
+    this.$el.html(yr.run("static", db.data["social"]));
+    VK.Widgets.Group("vk_groups1", {mode: 0, wide: 1, width: "300", height: "400", color1: "FFFFFF", color2: "2B587A", color3: "939393"}, 58259761);
+    VK.Widgets.Group("vk_groups2", {mode: 2, wide: 1, width: "430", height: "400", color1: "FFFFFF", color2: "2B587A", color3: "939393"}, 58259761);
+    $("#twitter-wjs").remove();
+    !function(d,s,id){
+      var js,
+          fjs = d.getElementsByTagName(s)[0],
+          p = /^http:/.test(d.location) ? "http" : "https";
+      if (!d.getElementById(id)) {
+        js = d.createElement(s);
+        js.id = id;
+        js.src = p + "://platform.twitter.com/widgets.js";
+        fjs.parentNode.insertBefore(js,fjs);
+      };
+    }(document,"script","twitter-wjs");
   }
 
 });
